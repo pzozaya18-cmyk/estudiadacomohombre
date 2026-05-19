734b7555f2c543b480e263675e03eec2c44a5896
pzozaya18-cmyk <pzozaya18@gmail.com>
2026-05-20 01:23:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './',
+})
+
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
